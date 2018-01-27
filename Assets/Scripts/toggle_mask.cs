03354684432451df6f08e5777f86caa2d265f523
ChrissiQ <rediahs@gmail.com>
2018-01-27 14:11:17 -0700

    Add waveform mask and trigger mask when pressing buttons.

---
+++ Assets/Scripts/toggle_mask.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class toggle_mask : MonoBehaviour {
+
+	public Image mask;
+
+	// Use this for initialization
+	void Start () {
+		Button button = GetComponent<Button>();
+		button.onClick.AddListener( () => {
+			mask.enabled = true;
+		} );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
