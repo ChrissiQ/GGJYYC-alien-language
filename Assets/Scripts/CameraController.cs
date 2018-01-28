12e125f5f5bf375d4edf6752d126853238647d4f
Rikki Prince <rfp@ecs.soton.ac.uk>
2018-01-27 18:58:10 -0700

    Moved my 3D adventure scene into the main project.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	[SerializeField] private Transform target;
+	[SerializeField] private Vector3 offset;
+	private float currentZoom = 10f;
+	[SerializeField] private float pitch = 2f;
+
+	void LateUpdate() {
+		// Determine angle of the player, so the camera can be rotated around the player.
+		float desiredAngle = target.transform.eulerAngles.y;
+		Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+
+		// Move position to the player
+		transform.position = target.position - (rotation * offset * currentZoom);
+		// Rotate to same angle as player.
+		// https://code.tutsplus.com/tutorials/unity3d-third-person-cameras--mobile-11230
+		transform.rotation = target.rotation;
+		// Orient camera to look at back of player's head.
+		transform.LookAt(target.position + Vector3.up * pitch);
+	}
+}
